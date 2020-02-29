333e08b0642f86c4f649708edb901831f5206883
Nick M <greekman100@gmail.com>
2020-02-28 16:08:03 -0800

    No longer removes quotes

--- FileToolkit.py
+++ FileToolkit.py
@@ -1,7 +1,18 @@
 #Import modules
 import Functions
+import os
 
 run = "y"
 
 while run == "y":
+        if os.name == "nt":
+                os.system("cls")
+        else:
+                os.system("clear")
+
         Functions.splitcsv()
+
+        print("\n\n\nEnd of program. Would you like to run another task?")
+        print("(Y/N) ", end="")
+        run = input().lower()
+
--- Functions.py
+++ Functions.py
@@ -10,6 +10,7 @@ def splitcsv():
 
     # List all CSV files in the current folder
     while True:
+        #print("topppppp\n\n")
         file_list = []
         counter = 0
         for files in os.listdir("./"):
@@ -36,14 +37,24 @@ def splitcsv():
             file_choice = int(input())
             print("You chose file:", file_list[file_choice - 1])
         except:
-            print("ERROR: daaaaaaaaaaaaaaa")
+            print("ERROR: Please enter a number between 1 and " + str(counter) + "\n\n\n")
         else:
             break
 
     # Get input from the user to choose how many lines per file before splitting
-    print("\nHow many lines in each output file before splitting: ", end="")
-    line_number_choice = int(input())
-    print("Each output file will write", line_number_choice, "lines before splitting")
+    while True:
+        print("\nHow many lines in each output file before splitting: ", end="")
+        try:
+            line_number_choice = int(input())
+            if (line_number_choice < 1):
+                print("ERROR: Please enter a number greater than 0")
+                continue
+            else:
+                print("Each output file will write", line_number_choice, "lines before splitting")
+        except:
+            print("ERROR: Please enter a number")
+        else:
+            break
 
     print("\nEnter a name for your output files: ", end="")
     OutputFileName = input()
@@ -51,17 +62,21 @@ def splitcsv():
 
     # Open the file the user picked from the list
     with open('%s' % file_list[file_choice - 1], 'r') as csvimport:
-        lines = csv.reader(csvimport, delimiter=',')
+        all_rows = csv.reader(csvimport, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONE)
         # Skip the first line (header information)
         next(csvimport)
-        for allrows in lines:
+
+        for row in all_rows:
             # If the max number of lines have been reached, create a new file
             if line_count == 0:
-                OutputFile = open(OutputFileName + '-' + str(output_file_number) + '.csv', 'w')
+                if (output_file_number < 10):
+                    OutputFile = open(OutputFileName + '-0' + str(output_file_number) + '.csv', 'w')
+                else:
+                    OutputFile = open(OutputFileName + '-' + str(output_file_number) + '.csv', 'w')
                 output_file_number = output_file_number + 1
 
             line_count = line_count + 1
-            OutputFile.write(','.join(allrows) + '\n')
+            OutputFile.write(','.join(row) + '\n')
             # If the max number of lines have been reached, close the file and reset the line count
             if line_count == line_number_choice:
                 OutputFile.close()
